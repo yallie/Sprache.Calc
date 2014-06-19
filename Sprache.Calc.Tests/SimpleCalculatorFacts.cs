95d9289dec4024386b439435bd264f7ca90eb30f
yallie <yallie@yandex.ru>
2014-06-19 09:52:17 +0400

    Created solution structure. Added SimpleCalculator class and its unit tests.

---
+++ Sprache.Calc.Tests/SimpleCalculatorFacts.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sprache.Calc.Tests
+{
+    public class SimpleCalculatorFacts
+    {
+		private SimpleCalculator calc = new SimpleCalculator();
+
+		[Fact]
+		public void DecimalWithoutLeadingDigitsIsSupported()
+		{
+			Assert.Equal(".01", calc.DecimalWithoutLeadingDigits.Parse(".01"));
+			Assert.Throws<ParseException>(() => calc.DecimalWithoutLeadingDigits.Parse("."));
+		}
+
+		[Fact]
+		public void DecimalWithLeadingDigitsIsSupported()
+		{
+			Assert.Equal("1", calc.DecimalWithLeadingDigits.Parse("1"));
+			Assert.Equal("1.01", calc.DecimalWithLeadingDigits.Parse("1.01"));
+			Assert.Throws<ParseException>(() => calc.DecimalWithLeadingDigits.Parse(".01"));
+		}
+
+		[Fact]
+		public void DecimalCanBeWithOrWithoutLeadingDigits()
+		{
+			Assert.Equal("2", calc.Decimal.Parse("2"));
+			Assert.Equal("3.14", calc.Decimal.Parse("3.14"));
+			Assert.Equal(".271828", calc.Decimal.Parse(".271828"));
+			Assert.Throws<ParseException>(() => calc.Decimal.End().Parse("0,1"));
+		}
+
+		private double GetValue(Expression x)
+		{
+			return (double)((ConstantExpression)x).Value;
+		}
+
+		[Fact]
+		public void ConstantReturnsDoubleValue()
+		{
+			Assert.Equal(1d, GetValue(calc.Constant.Parse("1")));
+			Assert.Equal(3.1415926d, GetValue(calc.Constant.Parse("3.1415926")));
+		}
+
+		[Fact]
+		public void OperatorParsesAnyStringAsGivenExpressionType()
+		{
+			Assert.Equal(ExpressionType.Power, calc.Operator("^", ExpressionType.Power).Parse("^"));
+			Assert.Equal(ExpressionType.AddAssign, calc.Operator("+=", ExpressionType.AddAssign).Parse("+="));
+			Assert.Throws<ParseException>(() => calc.Operator("*", ExpressionType.Multiply).Parse("+"));
+		}
+
+		[Fact]
+		public void ArithmeticOperationsAreSupported()
+		{
+			Assert.Equal(ExpressionType.AddChecked, calc.Add.Parse("+"));
+			Assert.Equal(ExpressionType.SubtractChecked, calc.Subtract.Parse("-"));
+			Assert.Equal(ExpressionType.MultiplyChecked, calc.Multiply.Parse("*"));
+			Assert.Equal(ExpressionType.Divide, calc.Divide.Parse("/"));
+			Assert.Equal(ExpressionType.Modulo, calc.Modulo.Parse("%"));
+			Assert.Equal(ExpressionType.Power, calc.Power.Parse("^"));
+			Assert.Throws<ParseException>(() => calc.Add.Parse("*"));
+		}
+    }
+}
---
+++ Sprache.Calc/SimpleCalculator.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprache.Calc
+{
+	/// <summary>
+	/// Simple calculator grammar. Suppors arithmetic operations.
+	/// </summary>
+	public class SimpleCalculator
+	{
+		protected internal virtual Parser<string> DecimalWithoutLeadingDigits
+		{
+			get
+			{
+				return
+					from dot in Parse.Char('.')
+					from fraction in Parse.Number
+					select dot + fraction;
+			}
+		}
+
+		protected internal virtual Parser<string> DecimalWithLeadingDigits
+		{
+			get { return Parse.Number.Then(n => DecimalWithoutLeadingDigits.XOr(Parse.Return(string.Empty)).Select(f => n + f)); }
+		}
+
+		protected internal virtual Parser<string> Decimal
+		{
+			get { return DecimalWithLeadingDigits.XOr(DecimalWithoutLeadingDigits); }
+		}
+
+		protected internal virtual Parser<Expression> Constant
+		{
+			get { return Decimal.Select(x => Expression.Constant(double.Parse(x, CultureInfo.InvariantCulture))).Named("Constant"); }
+		}
+
+		protected internal Parser<ExpressionType> Operator(string op, ExpressionType opType)
+		{
+			return Parse.String(op).Token().Return(opType);
+		}
+
+		protected internal virtual Parser<ExpressionType> Add
+		{
+			get { return Operator("+", ExpressionType.AddChecked); }
+		}
+
+		protected internal virtual Parser<ExpressionType> Subtract
+		{
+			get { return Operator("-", ExpressionType.SubtractChecked); }
+		}
+
+		protected internal virtual Parser<ExpressionType> Multiply
+		{
+			get { return Operator("*", ExpressionType.MultiplyChecked); }
+		}
+
+		protected internal virtual Parser<ExpressionType> Divide
+		{
+			get { return Operator("/", ExpressionType.Divide); }
+		}
+
+		protected internal virtual Parser<ExpressionType> Modulo
+		{
+			get { return Operator("%", ExpressionType.Modulo); }
+		}
+
+		protected internal virtual Parser<ExpressionType> Power
+		{
+			get { return Operator("^", ExpressionType.Power); }
+		}
+
+		protected virtual Parser<Expression> ExpressionInParentheses
+		{
+			get
+			{
+				return
+					from lparen in Parse.Char('(')
+					from expr in Expr
+					from rparen in Parse.Char(')')
+					select expr;
+			}
+		}
+
+		protected virtual Parser<Expression> Factor
+		{
+			get { return ExpressionInParentheses.XOr(Constant); }
+		}
+
+		protected virtual Parser<Expression> NegativeFactor
+		{
+			get
+			{
+				return
+					from sign in Parse.Char('-')
+					from factor in Factor
+					select Expression.NegateChecked(factor);
+			}
+		}
+
+		protected virtual Parser<Expression> Operand
+		{
+			get { return (NegativeFactor.XOr(Factor)).Token(); }
+		}
+
+		protected virtual Parser<Expression> InnerTerm
+		{
+			get { return Parse.ChainOperator(Power, Operand, Expression.MakeBinary); }
+		}
+
+		protected virtual Parser<Expression> Term
+		{
+			get { return Parse.ChainOperator(Multiply.Or(Divide).Or(Modulo), InnerTerm, Expression.MakeBinary); }
+		}
+
+		protected virtual Parser<Expression> Expr
+		{
+			get { return Parse.ChainOperator(Add.Or(Subtract), Term, Expression.MakeBinary); }
+		}
+
+		protected virtual Parser<Expression<Func<double>>> Lambda
+		{
+			get { return Expr.End().Select(body => Expression.Lambda<Func<double>>(body)); }
+		}
+
+		public virtual Expression<Func<double>> ParseExpression(string text)
+		{
+			return Lambda.Parse(text);
+		}
+	}
+}
